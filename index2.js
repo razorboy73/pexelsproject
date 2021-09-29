f673850602e2ec864b10ce9c171013298331d298
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2021-09-29 08:31:04 -0400

    finisehd project

--- index2.js
+++ index2.js
@@ -1,38 +1,44 @@
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-
+function getContent() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       // Typical action to be performed when the document is ready:
       let response = JSON.parse(xhttp.responseText);
-      console.log(response);
+
       let photoData = response.photos.map(function (photo) {
         return photo;
       });
 
-      var photoConent = document.getElementById("photo-content");
-      photoConent.innerHTML = "";
+      var heroImage = photoData[0].src.large;
+      console.log(heroImage);
+      document
+        .getElementById("hero-container")
+        .setAttribute(
+          "style",
+          "background-image: url(" + `${photoData[0].src.landscape}` + ")"
+        );
+
+      var bodyContainer = document.getElementById("body-container");
+      bodyContainer.innerHTML = "";
       photoData.forEach(function (photo) {
         console.log(photo);
         let photoContainerDiv = document.createElement("div");
         photoContainerDiv.classList.add("photo-container");
         photoContainerDiv.innerHTML = `
-                                              <a href=${photo.url}>
-                                              <img src="${photo.src.medium}" alt="" srcset="">
-                                              </a>
-                                              <div><h4>${photo.photographer}</h4></div>
-                                              `;
+                <a href=${photo.url}>
+                <img src="${photo.src.medium}" alt="" srcset="">
+                </a>
+                <div><h4>${photo.photographer}</h4></div>
+                `;
 
         // }
 
-        photoConent.appendChild(photoContainerDiv);
+        bodyContainer.appendChild(photoContainerDiv);
       });
     }
   };
-
   var searchValue = document.querySelector("#search-bar").value;
+
   xhttp.open(
     "GET",
     `https://api.pexels.com/v1/search?query=${searchValue}`,
@@ -44,4 +50,10 @@ searchForm.addEventListener("submit", function (e) {
     "563492ad6f91700001000001075a0265f4be495a9b94d6d0d5e5bc70"
   );
   xhttp.send();
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  getContent();
 });
